7a74ee870019963cb884a012db358760b207cef0
AlexZmasser <63077643+AlexZmasser@users.noreply.github.com>
2021-09-30 13:14:37 +1000

    Update

---
+++ Covid_database_code/Covid_database_code/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Covid_database_code
+//
+//  Created by Alex Zmasser on 30/9/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
