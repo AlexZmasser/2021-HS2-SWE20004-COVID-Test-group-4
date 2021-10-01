fb20fb1da216c27e2381201e06a2353a0fb6755e
AlexZmasser <63077643+AlexZmasser@users.noreply.github.com>
2021-10-01 13:19:13 +1000

    Update main
    changed the while loop and put the switch statements in the while loop so it will continue running until User exits the program

--- Code/main.cpp
+++ Code/main.cpp
@@ -22,11 +22,11 @@ ofstream locationDatabase;      //Opening a file in write mode.
 int main()  //Main function !ACTTING MENU!.
 {
     string operation;   //Declearing veriable.
-    int op;             //Declearing veriable.
+    int op = 0;             //Declearing veriable.
     int i = 0;          //Declearing and assigning veriable.
 
 
-    do //Do while loop to get into the loop before checking condition.
+    while (op!=6)
     {
         operation.clear();  //Clear the string.
         cout << "Press 1 - Enter Details for COVID-19 Test Recommendation" << endl
@@ -36,42 +36,45 @@ int main()  //Main function !ACTTING MENU!.
             << "Press 5 - Display Positive Patient Details" << endl
             << "Press 6 - Exit" << endl
             << "Enter Number: ";        //Output statement.
-        
-        getline(cin, operation);        //Read string that user inputs.
+        cin >> op;
+        cin.get();
+
+        /*getline(cin, operation);        //Read string that user inputs.
         stringstream toNum(operation);  //Object from class stringstream.
-        toNum >> op;                    //Object value of operation and stream it to integer op.
-        
+        toNum >> op; */                   //Object value of operation and stream it to integer op.
+
         if (1 > op || op > 6) //If input isn't within 1-6.
         {
             cout << endl << "Invalid Input Please Select 1-6" << endl << endl;  //Output statement.
             Sleep(1000);    //Waits 1 second.
         }
+        switch (op)                     //Switch case structure using value op
+        {
+        case 1:                         //if op = 1 case 1 will run. 
+            createPatient();            //Calling functions.
+            break;                      //Break from the case.
+        case 2:                         //if op = 2 case 2 will run. 
+            covidResult();              //Calling functions.
+            break;                      //Break from the case.
+        case 3:                         //if op = 3 case 3 will run. 
+            displayHotSpots();          //Calling functions.
+            break;                      //Break from the case.
+        case 4:                         //if op = 4 case 4 will run. 
+            updatePatient();            //Calling functions.
+            break;                      //Break from the case.
+        case 5:                         //if op = 5 case 5 will run.
+            infectedRecord();           //Calling functions.
+            break;                      //Break from the case.
+        case 6:
+            cout << "Closing, Goodbye..." << endl;          //Output statement.
+            break;                                          //Break from case.
+        default:                                            //if none of the cases matches op then this will.
+            cout << "Error 1: [Inside Default]" << endl;    //Error statement.
+        }
 
-    } while (!(1 <= op && op <= 6));   //Condition to repeat loop if number are outside range.
+    }    //Condition to repeat loop if number are outside range.
 
-    switch (op)                     //Switch case structure using value op
-    {
-    case 1:                         //if op = 1 case 1 will run. 
-        createPatient();            //Calling functions.
-        break;                      //Break from the case.
-    case 2:                         //if op = 2 case 2 will run. 
-        covidResult();              //Calling functions.
-        break;                      //Break from the case.
-    case 3:                         //if op = 3 case 3 will run. 
-        displayHotSpots();          //Calling functions.
-        break;                      //Break from the case.
-    case 4:                         //if op = 4 case 4 will run. 
-        updatePatient();            //Calling functions.
-        break;                      //Break from the case.
-    case 5:                         //if op = 5 case 5 will run.
-        infectedRecord();           //Calling functions.
-        break;                      //Break from the case.
-    case 6:
-        cout << "Closing, Goodbye..." << endl;          //Output statement.
-        break;                                          //Break from case.
-    default:                                            //if none of the cases matches op then this will.
-        cout << "Error 1: [Inside Default]" << endl;    //Error statement.
-    }
+
 
     system("pause");    //Pauses batch file.
     return 0;           //Return integer to the functions.
@@ -79,12 +82,12 @@ int main()  //Main function !ACTTING MENU!.
 
 void createPatient()   //Function.
 {
-    
+
 }
 
 void covidResult()     //Function.
 {
-    
+
 }
 
 void displayHotSpots() //Function.
@@ -101,3 +104,4 @@ void infectedRecord()  //Function.
 {
 
 }
+
